d3b06771dad87677e1d8fe84b3ec631de08befcf
alishaburgfeld <alisha.burgfeld@gmail.com>
2022-05-27 15:53:59 -0400

    added JS and PY

--- js/anagram2.js
+++ js/anagram2.js
@@ -1,3 +1,12 @@
 exports.anagramsFor = function(word, listOfWords) {
-
+    // anagramsFor = function(word, listOfWords) {
+    let wordArr= word.split("")
+    // console.log(wordArr)
+    let answer= listOfWords.filter(function (item) {
+        return wordArr.every(function (letter) {
+            return item.includes(letter)
+        })
+    })
+    return answer
 };
+// console.log(anagramsFor("threasd", ["threads", "trashed", "hardest", "hatreds", "hounds"]))
--- python/anagram2.py
+++ python/anagram2.py
@@ -1,4 +1,8 @@
 # Don't forget to run the tests (and create some of your own)
 
 def anagrams_for(word, list_of_words):
-		# your code here
+	answer=list()
+	for item in list_of_words:
+		if sorted(list(item))==sorted(list(word)):
+			answer.append(item)
+	return answer
